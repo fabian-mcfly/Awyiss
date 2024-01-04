fbceb6c4d28345d88e0c3544622c773f13ab23f8
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-01-04 20:12:45 +0100

    Removed customer dir for now

--- foobar-customer/config/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php declare(strict_types=1);
-
-\Cake\Utility\Text::setTransliteratorId('de-ASCII; Any-Latin; Latin-ASCII; [\u0080-\u7fff] remove');
--- foobar-customer/config/development/constants.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-
-defined('PAGEROLE_NEWS') || define('PAGEROLE_NEWS', 4);
-defined('PAGEROLE_PROMOTION') || define('PAGEROLE_PROMOTION', 3);
-defined('PAGEROLE_PAGE') || define('PAGEROLE_PAGE', 1);
-defined('PAGEROLE_PUBLICATION') || define('PAGEROLE_PUBLICATION', 32);
