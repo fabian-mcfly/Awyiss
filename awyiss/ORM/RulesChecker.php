f882de676ae359cae8d89a802a19f646133c84cc
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-01-17 17:35:57 +0100

    RulesChecker: build rules with their names and make sure it's unique

--- awyiss/ORM/RulesChecker.php
+++ awyiss/ORM/RulesChecker.php
@@ -9,6 +9,8 @@ use Cake\Datasource\RuleInvoker;
 use Cake\ORM\Association;
 use Cake\ORM\RulesChecker as BaseRulesChecker;
 use Cake\ORM\Table as BaseTable;
+use Cake\Utility\Hash;
+use RuntimeException;
 
 
 /**
@@ -24,28 +26,92 @@ use Cake\ORM\Table as BaseTable;
 class RulesChecker extends BaseRulesChecker {
 	/**
 	 * @inheritDoc
-	 * @param array $aa_fields
-	 * @param array|string|null $ax_options
-	 * @return RuleInvoker
 	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
 	 */
-	public function isUnique(array $aa_fields, array|string|null $ax_options = null): RuleInvoker {
-		$la_options = is_array($ax_options) ? $ax_options : ['message' => $ax_options];
+	public function add(callable $ao_rule, array|string|null $ax_name = null, array $aa_options = []) {
+		$ls_name = is_string($ax_name) ? $ax_name : $this->extractName($ax_name);
 
-		if (empty($la_options['message'])) {
-			$la_options['message'] = __d('validation', 'error_unique');
+		if (isset($this->_rules[ $ls_name ])) {
+			throw new RuntimeException(sprintf('Cannot redefine rule name. `%s` already exists.', $ls_name));
 		}
 
-		$la_fields = $aa_fields;
-		if (($this->_options['repository'] ?? null)) {
-			$ls_entityClass = $this->_options['repository']->getEntityClass() ?? null;
-			if ($ls_entityClass) {
-				$la_fields = $ls_entityClass::unmapFields($la_fields);
-			}
+		$this->_rules[ $ls_name ] = $this->_addError($ao_rule, $ls_name, $aa_options);
+
+
+		return $this;
+	}
+
+
+	/**
+	 * @inheritDoc
+	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
+	 */
+	public function addCreate(callable $ao_rule, array|string|null $ax_name = null, array $aa_options = []) {
+		$ls_name = is_string($ax_name) ? $ax_name : $this->extractName($ax_name);
+
+		if (isset($this->_createRules[ $ls_name ]) || isset($this->_rules[ $ls_name ])) {
+			throw new RuntimeException(sprintf('Cannot redefine create rule name. `%s` already exists.', $ls_name));
 		}
 
+		$this->_createRules[ $ls_name ] = $this->_addError($ao_rule, $ls_name, $aa_options);
 
-		return parent::isUnique($la_fields, $la_options);
+
+		return $this;
+	}
+
+
+	/**
+	 * @inheritDoc
+	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
+	 */
+	public function addUpdate(callable $ao_rule, array|string|null $ax_name = null, array $aa_options = []) {
+		$ls_name = is_string($ax_name) ? $ax_name : $this->extractName($ax_name);
+
+		if (isset($this->_updateRules[ $ls_name ]) || isset($this->_rules[ $ls_name ])) {
+			throw new RuntimeException(sprintf('Cannot redefine update rule name. `%s` already exists.', $ls_name));
+		}
+
+		$this->_updateRules[ $ls_name ] = $this->_addError($ao_rule, $ls_name, $aa_options);
+
+
+		return $this;
+	}
+
+
+	/**
+	 * @inheritDoc
+	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
+	 */
+	public function addDelete(callable $ao_rule, array|string|null $ax_name = null, array $aa_options = []) {
+		$ls_name = is_string($ax_name) ? $ax_name : $this->extractName($ax_name);
+
+		if (isset($this->_deleteRules[ $ls_name ])) {
+			throw new RuntimeException(sprintf('Cannot redefine delete rule name. `%s` already exists.', $ls_name));
+		}
+
+		$this->_deleteRules[ $ls_name ] = $this->_addError($ao_rule, $ls_name, $aa_options);
+
+
+		return $this;
+	}
+
+
+	/**
+	 * Returns whether a rule with the given name exists in any of the possible rulesets
+	 *
+	 * @param string $as_ruleName
+	 * @return bool
+	 */
+	public function exists(string $as_ruleName): bool {
+		/** @var \Cake\Datasource\RuleInvoker[] $la_rules */
+		$la_rules = Hash::merge(
+			$this->_rules,
+			$this->_createRules,
+			$this->_updateRules,
+			$this->_deleteRules,
+		);
+
+		return array_key_exists($as_ruleName, $la_rules);
 	}
 
 
@@ -72,6 +138,33 @@ class RulesChecker extends BaseRulesChecker {
 
 		//return parent::existsIn($ax_fields, $ax_table, $la_options);
 		return $this->_addError(new ExistsIn($ax_fields, $ax_table, $la_options), '_existsIn', ['errorField' => $ls_errorField, 'message' => $ls_message]);
+	}
+
+
+	/**
+	 * @inheritDoc
+	 * @param array $aa_fields
+	 * @param array|string|null $ax_options
+	 * @return RuleInvoker
+	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
+	 */
+	public function isUnique(array $aa_fields, array|string|null $ax_options = null): RuleInvoker {
+		$la_options = is_array($ax_options) ? $ax_options : ['message' => $ax_options];
+
+		if (empty($la_options['message'])) {
+			$la_options['message'] = __d('validation', 'error_unique');
+		}
+
+		$la_fields = $aa_fields;
+		if (($this->_options['repository'] ?? null)) {
+			$ls_entityClass = $this->_options['repository']->getEntityClass() ?? null;
+			if ($ls_entityClass) {
+				$la_fields = $ls_entityClass::unmapFields($la_fields);
+			}
+		}
+
+
+		return parent::isUnique($la_fields, $la_options);
 	}
 
 
@@ -126,6 +219,22 @@ class RulesChecker extends BaseRulesChecker {
 			$ls_message = __d('validation', 'error_link_constraint_rule', $ls_associationAlias);
 		}
 
+
 		return parent::_addLinkConstraintRule($ax_association, $as_errorField, $ls_message, $as_linkStatus, $as_ruleName);
+	}
+
+
+	/**
+	 * Extract the name from the options array
+	 *
+	 * @param array|null $aa_options
+	 * @return mixed
+	 */
+	protected function extractName(?array $aa_options) {
+		if ($aa_options === NULL || empty($aa_options['name'])) {
+			throw new RuntimeException(sprintf('Missing option `name` in `%s`', static::class));
+		}
+
+		return $aa_options['name'];
 	}
 }
