4f43e7b25d2035bf24a033530b8edb4fac91f4b1
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-02-20 20:13:23 +0100

    LocalConfig: added write method

--- awyiss/Core/LocalConfig.php
+++ awyiss/Core/LocalConfig.php
@@ -7,6 +7,7 @@ namespace Awyiss\Core;
 use Awyiss\Awyiss;
 use Awyiss\Routing\Router;
 use Cake\Core\Configure;
+use Cake\Utility\Hash;
 
 
 /**
@@ -15,21 +16,18 @@ use Cake\Core\Configure;
  */
 class LocalConfig extends Configure {
 	/**
-	 * Used to read information stored in Configure. It's not
-	 * possible to store `null` values in Configure.
-	 * Usage:
-	 * ```
-	 * Configure::read('Name'); will return all values for Name
-	 * Configure::read('Name.key'); will return only the value of Configure::Name[key]
-	 * ```
+	 * Used to read information stored in Configure for a given scope.
+	 * Defaults to the current controller.
 	 *
-	 * @param string|null $var Variable to obtain. Use '.' to access array elements.
-	 * @param mixed $default The return value when the configure does not exist
-	 * @return mixed Value stored in configure, or null.
-	 * @link https://book.cakephp.org/5/en/development/configuration.html#reading-configuration-data
+	 * @param array|string|null $ax_path
+	 * @param mixed|null $ax_default
+	 * @param string|null $as_scope
+	 * @return mixed
 	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
 	 */
-	public static function read(string|array|null $ax_path = null, mixed $ax_default = null): mixed {
+	public static function read(string|array|null $ax_path = null, mixed $ax_default = null, ?string $as_scope = null): mixed {
+		$ls_scope = $as_scope ?? Router::getRequest()->getParam('controller');
+
 		if ($ax_path) {
 			$la_path = is_array($ax_path) ? $ax_path : explode('.', $ax_path);
 		}
@@ -37,17 +35,51 @@ class LocalConfig extends Configure {
 			$la_path = [];
 		}
 
-		$ls_controller = Router::getRequest()->getParam('controller');
-
 		array_unshift(
 			$la_path,
 			'Awyiss',
-			$ls_controller,
+			$ls_scope,
 			Awyiss::getRealm()
 		);
 
 
 		return parent::read(static::stringify($la_path), $ax_default);
+	}
+
+
+	/**
+	 * Used to store a dynamic variable in Configure for a given scope.
+	 * Defaults to the current controller.
+	 *
+	 * @param array|string $ax_config
+	 * @param mixed|null $ax_value
+	 * @param string|null $as_scope
+	 * @return void
+	 * @noinspection PhpParameterNameChangedDuringInheritanceInspection
+	 */
+	public static function write(array|string $ax_config, mixed $ax_value = null, ?string $as_scope = null): void {
+		$ls_scope = $as_scope ?? Router::getRequest()->getParam('controller');
+
+		$la_config = $ax_config;
+		if (!is_array($ax_config)) {
+			$la_config = [$ax_config => $ax_value];
+		}
+
+		$la_localConfig = [];
+		foreach ($la_config as $ls_key => $lx_value) {
+			$la_path = explode('.', $ls_key);
+
+			array_unshift(
+				$la_path,
+				'Awyiss',
+				$ls_scope,
+				Awyiss::getRealm()
+			);
+
+			$la_localConfig = Hash::merge($la_localConfig, [static::stringify($la_path) => $lx_value]);
+		}
+
+		parent::write($la_localConfig, $ax_value);
 	}
 
 
