c1ffe8fecf70704a53f2315d96c7447f2f154af8
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-06-13 13:57:56 +0200

    Extended the cakephp Inflector with a `ucparts`-method

---
+++ awyiss/Utility/Inflector.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+
+namespace Awyiss\Utility;
+
+
+use Cake\Utility\Inflector as CakeInflector;
+
+
+/**
+ * Inflector
+ *
+ * @package Awyiss\Utility
+ */
+class Inflector extends CakeInflector {
+	/**
+	 * Converts strings delimited by '-', '\', '_' and ' ' to have each part capitalized,
+	 * either keeping the delimiter or replacing it with a custom delimiter.
+	 *
+	 * If $delimiter is set to false, the delimiter will be removed.
+	 *
+	 * Example:
+	 * ```
+	 * Inflector::ucparts('foo-bar');
+	 * // 'Foo-Bar'
+	 *
+	 * Inflector::ucparts('foo-bar', '_');
+	 * // 'Foo_Bar'
+	 *
+	 * Inflector::ucparts('foo-bar', false);
+	 * // 'FooBar'
+	 * ```
+	 *
+	 * @param string $string
+	 * @param string|bool $delimiter
+	 * @return string
+	 */
+	public static function ucparts(string $string, string|bool $delimiter = true): string {
+		$ls_string = ucwords(strtolower($string));
+
+		foreach (['-', '\'', '_', ' '] as $ls_delimiter) {
+			if (strpos($ls_string, $ls_delimiter) === false) {
+				continue;
+			}
+
+			$ls_string = implode(
+				$delimiter ? (is_string($delimiter) ? $delimiter : $ls_delimiter) : '',
+				array_map('ucfirst', explode($ls_delimiter, $ls_string))
+			);
+		}
+
+		return $ls_string;
+	}
+}
