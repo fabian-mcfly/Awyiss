6ef4c1124a5c82bc485b27e79123fb2f8b773be0
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-06-14 13:12:01 +0200

    Register the globally used middleware in routes.php

--- awyiss/config/routes.php
+++ awyiss/config/routes.php
@@ -2,8 +2,72 @@
 
 
 use Awyiss\Awyiss;
+use Awyiss\Middleware\ConfigMiddleware;
+use Awyiss\Middleware\DesignMiddleware;
+use Awyiss\Middleware\EventListenersMiddleware;
+use Awyiss\Middleware\LocaleMiddleware;
 use Awyiss\Routing\Route\AwyissRoute;
+use Cake\Core\Configure;
+use Cake\Http\Middleware\CspMiddleware;
 use Cake\Routing\RouteBuilder;
+
+
+/** @var RouteBuilder $routes */
+
+$routes->registerMiddleware('config', new ConfigMiddleware());
+$routes->registerMiddleware(
+	'csp',
+	new CspMiddleware(
+		[
+			'connect-src' => [
+				'self' => true,
+				'blob' => true,
+			],
+			'default-src' => [],
+			'font-src' => [
+				'self' => true,
+			],
+			'frame-src' => [
+				'self' => true,
+			],
+			'img-src' => [
+				'blob' => true,
+				'self' => true,
+				'data' => true
+			],
+			'script-src' => [
+				'allow' => Configure::read('Csp.scriptSrc.allow'),
+				'self' => true,
+				'unsafe-inline' => false,
+				'unsafe-eval' => false,
+			],
+			'style-src' => [
+				'allow' => Configure::read('Csp.styleSrc.allow'),
+				'self' => true,
+				'unsafe-inline' => true,
+				'unsafe-eval' => false,
+			],
+			'style-src-attr' => [
+				'allow' => Configure::read('Csp.styleSrc.allow'),
+				'self' => true,
+				'unsafe-inline' => true,
+				'unsafe-eval' => false,
+			],
+			'style-src-elem' => [
+				'allow' => Configure::read('Csp.styleSrc.allow'),
+				'self' => true,
+				'unsafe-inline' => true,
+				'unsafe-eval' => false,
+			],
+		], [
+			'scriptNonce' => true,
+			'styleNonce' => true,
+		]
+	)
+);
+$routes->registerMiddleware('eventListeners', new EventListenersMiddleware());
+$routes->registerMiddleware('design', new DesignMiddleware());
+$routes->registerMiddleware('requestLocale', new LocaleMiddleware());
 
 
 /** @var RouteBuilder $routes */
