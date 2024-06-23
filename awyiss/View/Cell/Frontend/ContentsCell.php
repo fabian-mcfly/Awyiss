5230ac1b0c4b1ecb6020ea693e147e46d7c0453b
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-06-23 12:46:07 +0200

    Combined the shared logic of widgets and contents in a trait

--- awyiss/View/Cell/Frontend/ContentsCell.php
+++ awyiss/View/Cell/Frontend/ContentsCell.php
@@ -4,15 +4,10 @@
 namespace Awyiss\View\Cell\Frontend;
 
 
+use Awyiss\Model\Entity;
 use Awyiss\Model\Entity\Content;
 use Awyiss\Model\Entity\Page;
-use Awyiss\Utility\Inflector;
-use Awyiss\View\FrontendView;
-use Cake\Collection\CollectionInterface;
-use Cake\Core\Configure;
-use Cake\Event\EventManagerInterface;
-use Cake\Http\Response;
-use Cake\Http\ServerRequest;
+use Awyiss\View\Cell\Frontend\Trait\ContentElementTrait;
 use Cake\View\Cell;
 
 
@@ -22,17 +17,7 @@ use Cake\View\Cell;
  * Outputs the contents of a content area for a specific page.
  */
 class ContentsCell extends Cell {
-	protected FrontendView $view;
-
-
-	/**
-	 * @inheritDoc
-	 */
-	public function __construct(ServerRequest $request, Response $response, ?EventManagerInterface $eventManager = null, array $cellOptions = []) {
-		parent::__construct($request, $response, $eventManager, $cellOptions);
-
-		$this->view = new FrontendView($request, $response);
-	}
+	use ContentElementTrait;
 
 
 	/**
@@ -84,9 +69,9 @@ class ContentsCell extends Cell {
 			return $content->parentId === null;
 		})->compile();
 
-		$this->prepareContents($lo_contents, (float)$la_options['columnWidth']);
+		$this->prepareEntities($lo_contents, (float)$la_options['columnWidth']);
 
-		$ls_contents = $this->buildContent($lo_contents->toArray());
+		$ls_contents = $this->buildContents($lo_contents->toArray());
 
 		// Set the view variables
 		$this->set([
@@ -100,258 +85,15 @@ class ContentsCell extends Cell {
 
 
 	/**
-	 * @param \Cake\Collection\CollectionInterface $contents
-	 * @param float $columnWidth
-	 */
-	protected function prepareContents(CollectionInterface $contents, float $columnWidth = 100.00): void {
-		if (!$contents->count()) {
-			return;
-		}
-
-		$lo_contents = $contents->listNested();
-		$lo_lastContent = null;
-		$la_parentContents = [];
-
-		/** @var \Awyiss\Model\Entity\Content $lo_content */
-		foreach ($lo_contents as $lo_content) {
-			$lo_content->setVirtual(['level']);
-			//Add the current depth as a level-property to the entity
-			/** @noinspection PhpPossiblePolymorphicInvocationInspection */
-			$lo_content->level = $lo_contents->getDepth();
-
-			// Remember the parent contents
-			if ($lo_lastContent) {
-				if ($lo_lastContent->level < $lo_content->level) {
-					$la_parentContents[] = $lo_lastContent;
-				}
-				elseif ($lo_lastContent->level > $lo_content->level) {
-					$la_parentContents = array_slice($la_parentContents, 0, $lo_content->level);
-				}
-			}
-
-			$lo_content->parentContents = $la_parentContents;
-
-			// Set the cssClass property
-			$this->setCssClasses($lo_content);
-
-			// Seat the real column width
-			$this->setRealColumnWidth($lo_content, $columnWidth);
-
-			// Set the template for the content
-			// Will use a custom template named "Content<id>.twig", if it exists.
-			$this->setTemplate($lo_content);
-
-			$lo_lastContent = $lo_content;
-		}
-	}
-
-
-	/**
-	 * Render all provided contents, including children
-	 *
-	 * @param array $contents
-	 * @return string
-	 */
-	protected function buildContent(array $contents): string {
-		if (!$contents) {
-			return '';
-		}
-
-		$ls_contents = '';
-		$lf_currentWidth = 0;
-		$ls_rowContents = '';
-
-		/**
-		 * @var \Awyiss\Model\Entity\Content $lo_content
-		 */
-		foreach ($contents as $lo_content) {
-			$ls_children = '';
-
-			if ($lo_content->children) {
-				$ls_children = $this->buildContent($lo_content->children);
-			}
-
-			/*
-			 * If the content template should not be rendered in a content row
-			 * render the content directly, but not before rendering existing row contents.
-			 */
-			if (!$lo_content->contentTemplate->inContentRow) {
-				if ($ls_rowContents) {
-					$ls_contents .= $this->renderContentRow($ls_rowContents);
-
-					// Reset the row contents
-					$lf_currentWidth = 0;
-					$ls_rowContents = '';
-				}
-
-				// Render the content. Adding the width is not necessary, as the content is rendered directly.
-				$ls_contents .= $this->renderContent($lo_content, $ls_children);
-
-				continue;
-			}
-
-			// Get the real column width of the content
-			$lf_columnWidth = 100 * $lo_content->column['width']->getFactor();
-
-			// If the content has a column indent, add the width of the indent to the current column width
-			if ($lo_content->column['indent']) {
-				$lf_columnWidth += 100 * $lo_content->column['indent']->getFactor();
-			}
-
-			/*
-			 * Before rendering the content, check if the content row is full,
-			 * or if the content - including potential indentation - would exceed the row width.
-			 * If that is the case, render the current row and reset the row contents.
-			 */
-			if ($lf_currentWidth > 100 || $lf_currentWidth + $lf_columnWidth > 100) {
-				if ($ls_rowContents) {
-					$ls_contents .= $this->renderContentRow($ls_rowContents);
-				}
-
-				$lf_currentWidth = 0;
-				$ls_rowContents = '';
-			}
-
-			// Add the content to the row contents
-			$ls_rowContents .= $this->renderContent($lo_content, $ls_children);
-
-			// If the content is a finisher, render the row and reset the row contents
-			if ($lo_content->columnLast) {
-				$ls_contents .= $this->renderContentRow($ls_rowContents);
-
-				$lf_currentWidth = 0;
-				$ls_rowContents = '';
-			}
-			else {
-				// Add the width of the content to the current row width
-				$lf_currentWidth += $lf_columnWidth;
-			}
-		}
-
-		// Render the last row
-		if ($ls_rowContents) {
-			$ls_contents .= $this->renderContentRow($ls_rowContents);
-		}
-
-
-		return $ls_contents;
-	}
-
-
-	/**
-	 * @param \Awyiss\Model\Entity\Content $content
+	 * @inheritDoc
+	 * @param \Awyiss\Model\Entity\Content $entity
 	 * @param string $children
 	 * @return string
 	 */
-	protected function renderContent(Content $content, string $children): string {
-		$ls_content = $this->view->content($content->contentTemplate->fileName, [
-			'content' => $content,
+	protected function renderContentElement(Entity $entity, string $children): string {
+		return $this->view->content($entity->contentTemplate->fileName, [
+			'content' => $entity,
 			'children' => $children,
 		]);
-
-		return $ls_content;
-	}
-
-
-	/**
-	 * @param string $contents
-	 * @return string
-	 */
-	protected function renderContentRow(string $contents): string {
-		$ls_contentRow = $this->view->element('content_row', [
-			'contents' => $contents,
-			'class' => $this->view::$rowClass,
-		]);
-
-		$this->view::$rowClass = '';
-
-		return $ls_contentRow;
-	}
-
-
-	/**
-	 * Set the real column width of a content,
-	 * based on the width of its parent content.
-	 *
-	 * @param \Awyiss\Model\Entity\Content $content
-	 * @param float $columnWidth
-	 * @return void
-	 */
-	protected function setRealColumnWidth(Content $content, float $columnWidth): void {
-		$content->setVirtual(['realColumnWidth']);
-
-		if (!$content->parentContents) {
-			$content->realColumnWidth = $columnWidth * $content->column['width']->getFactor();
-
-			return;
-		}
-
-		/**
-		 * @var \Awyiss\Model\Entity\Content $lo_parentContent
-		 */
-		$lo_parentContent = end($content->parentContents);
-
-		$content->realColumnWidth = $lo_parentContent->realColumnWidth * $content->column['width']->getFactor();
-	}
-
-
-	/**
-	 * Set the cssClass property
-	 * Each content element gets a css class based on its content template, column width and indent,
-	 * as well as the cssClass property set in the database.
-	 *
-	 * @param \Awyiss\Model\Entity\Content $content
-	 * @return void
-	 */
-	protected function setCssClasses(Content $content): void {
-		if (empty($content->cssClass)) {
-			$content->cssClass = '';
-		}
-
-		$ls_cssClass = trim($content->cssClass);
-
-		$content->cssClass = 'ContentElement Template-' . Inflector::ucparts($content->contentTemplate->fileName);
-		$content->cssClass .= ' ' . $content->column['width']->getCssClass();
-
-		if ($content->column['indent']) {
-			$content->cssClass .= ' ' . $content->column['indent']?->getCssClass();
-		}
-
-		if ($content->columnRtl) {
-			$content->cssClass .= ' Column-Rtl';
-		}
-
-		if ($ls_cssClass) {
-			$content->cssClass .= ' ' . $ls_cssClass;
-		}
-	}
-
-
-	/**
-	 * Check if a custom template exists for a content element,
-	 * based on the content id.
-	 *
-	 * @param \Awyiss\Model\Entity\Content $content
-	 * @return void
-	 */
-	protected function setTemplate(Content $content): void {
-		static $ls_templatePath;
-
-		if (!isset($ls_templatePath)) {
-			$ls_templatePath = rtrim(Configure::read('App.paths.templates.customer'), DS);
-		}
-
-		$ls_fileName = 'Content' . $content->id;
-
-		$ls_filePath = implode(DS, [
-			$ls_templatePath,
-			'Frontend',
-			'content',
-			$ls_fileName . '.twig',
-		]);
-
-		if (file_exists($ls_filePath)) {
-			$content->contentTemplate->fileName = $ls_fileName;
-		}
 	}
 }
--- awyiss/View/Cell/Frontend/WidgetsCell.php
+++ awyiss/View/Cell/Frontend/WidgetsCell.php
@@ -4,14 +4,9 @@
 namespace Awyiss\View\Cell\Frontend;
 
 
+use Awyiss\Model\Entity;
 use Awyiss\Model\Entity\Widget;
-use Awyiss\Utility\Inflector;
-use Awyiss\View\FrontendView;
-use Cake\Collection\CollectionInterface;
-use Cake\Core\Configure;
-use Cake\Event\EventManagerInterface;
-use Cake\Http\Response;
-use Cake\Http\ServerRequest;
+use Awyiss\View\Cell\Frontend\Trait\ContentElementTrait;
 use Cake\View\Cell;
 
 
@@ -21,17 +16,7 @@ use Cake\View\Cell;
  * Outputs the widgets for a given identifier
  */
 class WidgetsCell extends Cell {
-	protected FrontendView $view;
-
-
-	/**
-	 * @inheritDoc
-	 */
-	public function __construct(ServerRequest $request, Response $response, ?EventManagerInterface $eventManager = null, array $cellOptions = []) {
-		parent::__construct($request, $response, $eventManager, $cellOptions);
-
-		$this->view = new FrontendView($request, $response);
-	}
+	use ContentElementTrait;
 
 
 	/**
@@ -79,9 +64,9 @@ class WidgetsCell extends Cell {
 			return $widget->parentId === null;
 		})->compile();
 
-		$this->prepareWidgets($lo_widgets, (float)$la_options['columnWidth']);
+		$this->prepareEntities($lo_widgets, (float)$la_options['columnWidth']);
 
-		$ls_widgets = $this->buildWidget($lo_widgets->toArray());
+		$ls_widgets = $this->buildContents($lo_widgets->toArray());
 
 		// Set the view variables
 		$this->set([
@@ -93,258 +78,15 @@ class WidgetsCell extends Cell {
 
 
 	/**
-	 * @param \Cake\Collection\CollectionInterface $widgets
-	 * @param float $columnWidth
-	 */
-	protected function prepareWidgets(CollectionInterface $widgets, float $columnWidth = 100.00): void {
-		if (!$widgets->count()) {
-			return;
-		}
-
-		$lo_widgets = $widgets->listNested();
-		$lo_lastWidget = null;
-		$la_parentWidgets = [];
-
-		/** @var \Awyiss\Model\Entity\Widget $lo_widget */
-		foreach ($lo_widgets as $lo_widget) {
-			$lo_widget->setVirtual(['level']);
-			//Add the current depth as a level-property to the entity
-			/** @noinspection PhpPossiblePolymorphicInvocationInspection */
-			$lo_widget->level = $lo_widgets->getDepth();
-
-			// Remember the parent widgets
-			if ($lo_lastWidget) {
-				if ($lo_lastWidget->level < $lo_widget->level) {
-					$la_parentWidgets[] = $lo_lastWidget;
-				}
-				elseif ($lo_lastWidget->level > $lo_widget->level) {
-					$la_parentWidgets = array_slice($la_parentWidgets, 0, $lo_widget->level);
-				}
-			}
-
-			$lo_widget->parentWidgets = $la_parentWidgets;
-
-			// Set the cssClass property
-			$this->setCssClasses($lo_widget);
-
-			// Seat the real column width
-			$this->setRealColumnWidth($lo_widget, $columnWidth);
-
-			// Set the template for the widget
-			// Will use a custom template named "Widget<id>.twig", if it exists.
-			$this->setTemplate($lo_widget);
-
-			$lo_lastWidget = $lo_widget;
-		}
-	}
-
-
-	/**
-	 * Render all provided widgets, including children
-	 *
-	 * @param array $widgets
-	 * @return string
-	 */
-	protected function buildWidget(array $widgets): string {
-		if (!$widgets) {
-			return '';
-		}
-
-		$ls_widgets = '';
-		$lf_currentWidth = 0;
-		$ls_rowContents = '';
-
-		/**
-		 * @var \Awyiss\Model\Entity\Widget $lo_widget
-		 */
-		foreach ($widgets as $lo_widget) {
-			$ls_children = '';
-
-			if ($lo_widget->children) {
-				$ls_children = $this->buildWidget($lo_widget->children);
-			}
-
-			/*
-			 * If the widget template should not be rendered in a content row
-			 * render the widget directly, but not before rendering existing row widgets.
-			 */
-			if (!$lo_widget->widgetTemplate->inContentRow) {
-				if ($ls_rowContents) {
-					$ls_widgets .= $this->renderContentRow($ls_rowContents);
-
-					// Reset the row widgets
-					$lf_currentWidth = 0;
-					$ls_rowContents = '';
-				}
-
-				// Render the widget. Adding the width is not necessary, as the widget is rendered directly.
-				$ls_widgets .= $this->renderWidget($lo_widget, $ls_children);
-
-				continue;
-			}
-
-			// Get the real column width of the widget
-			$lf_columnWidth = 100 * $lo_widget->column['width']->getFactor();
-
-			// If the widget has a column indent, add the width of the indent to the current column width
-			if ($lo_widget->column['indent']) {
-				$lf_columnWidth += 100 * $lo_widget->column['indent']->getFactor();
-			}
-
-			/*
-			 * Before rendering the widget, check if the content row is full,
-			 * or if the widget - including potential indentation - would exceed the row width.
-			 * If that is the case, render the current row and reset the row widgets.
-			 */
-			if ($lf_currentWidth > 100 || $lf_currentWidth + $lf_columnWidth > 100) {
-				if ($ls_rowContents) {
-					$ls_widgets .= $this->renderContentRow($ls_rowContents);
-				}
-
-				$lf_currentWidth = 0;
-				$ls_rowContents = '';
-			}
-
-			// Add the widget to the row widgets
-			$ls_rowContents .= $this->renderWidget($lo_widget, $ls_children);
-
-			// If the widget is a finisher, render the row and reset the row widgets
-			if ($lo_widget->columnLast) {
-				$ls_widgets .= $this->renderContentRow($ls_rowContents);
-
-				$lf_currentWidth = 0;
-				$ls_rowContents = '';
-			}
-			else {
-				// Add the width of the widget to the current row width
-				$lf_currentWidth += $lf_columnWidth;
-			}
-		}
-
-		// Render the last row
-		if ($ls_rowContents) {
-			$ls_widgets .= $this->renderContentRow($ls_rowContents);
-		}
-
-
-		return $ls_widgets;
-	}
-
-
-	/**
-	 * @param \Awyiss\Model\Entity\Widget $widget
+	 * @inheritDoc
+	 * @param \Awyiss\Model\Entity\Widget $entity
 	 * @param string $children
 	 * @return string
 	 */
-	protected function renderWidget(Widget $widget, string $children): string {
-		$ls_widget = $this->view->widget($widget->widgetTemplate->fileName, [
-			'widget' => $widget,
+	protected function renderContentElement(Entity $entity, string $children): string {
+		return $this->view->widget($entity->widgetTemplate->fileName, [
+			'widget' => $entity,
 			'children' => $children,
 		]);
-
-		return $ls_widget;
-	}
-
-
-	/**
-	 * @param string $widgets
-	 * @return string
-	 */
-	protected function renderContentRow(string $widgets): string {
-		$ls_contentRow = $this->view->element('content_row', [
-			'contents' => $widgets,
-			'class' => $this->view::$rowClass,
-		]);
-
-		$this->view::$rowClass = '';
-
-		return $ls_contentRow;
-	}
-
-
-	/**
-	 * Set the real column width of a widget,
-	 * based on the width of its parent widget.
-	 *
-	 * @param \Awyiss\Model\Entity\Widget $widget
-	 * @param float $columnWidth
-	 * @return void
-	 */
-	protected function setRealColumnWidth(Widget $widget, float $columnWidth): void {
-		$widget->setVirtual(['realColumnWidth']);
-
-		if (!$widget->parentWidgets) {
-			$widget->realColumnWidth = $columnWidth * $widget->column['width']->getFactor();
-
-			return;
-		}
-
-		/**
-		 * @var \Awyiss\Model\Entity\Widget $lo_parentWidget
-		 */
-		$lo_parentWidget = end($widget->parentWidgets);
-
-		$widget->realColumnWidth = $lo_parentWidget->realColumnWidth * $widget->column['width']->getFactor();
-	}
-
-
-	/**
-	 * Set the cssClass property
-	 * Each widget element gets a css class based on its widget template, column width and indent,
-	 * as well as the cssClass property set in the database.
-	 *
-	 * @param \Awyiss\Model\Entity\Widget $widget
-	 * @return void
-	 */
-	protected function setCssClasses(Widget $widget): void {
-		if (empty($widget->cssClass)) {
-			$widget->cssClass = '';
-		}
-
-		$ls_cssClass = trim($widget->cssClass);
-
-		$widget->cssClass = 'WidgetElement Template-' . Inflector::ucparts($widget->widgetTemplate->fileName);
-		$widget->cssClass .= ' ' . $widget->column['width']->getCssClass();
-
-		if ($widget->column['indent']) {
-			$widget->cssClass .= ' ' . $widget->column['indent']?->getCssClass();
-		}
-
-		if ($widget->columnRtl) {
-			$widget->cssClass .= ' Column-Rtl';
-		}
-
-		if ($ls_cssClass) {
-			$widget->cssClass .= ' ' . $ls_cssClass;
-		}
-	}
-
-
-	/**
-	 * Check if a custom template exists for a widget element,
-	 * based on the widget id.
-	 *
-	 * @param \Awyiss\Model\Entity\Widget $widget
-	 * @return void
-	 */
-	protected function setTemplate(Widget $widget): void {
-		static $ls_templatePath;
-
-		if (!isset($ls_templatePath)) {
-			$ls_templatePath = rtrim(Configure::read('App.paths.templates.customer'), DS);
-		}
-
-		$ls_fileName = 'Widget' . $widget->id;
-
-		$ls_filePath = implode(DS, [
-			$ls_templatePath,
-			'Frontend',
-			'widget',
-			$ls_fileName . '.twig',
-		]);
-
-		if (file_exists($ls_filePath)) {
-			$widget->widgetTemplate->fileName = $ls_fileName;
-		}
 	}
 }
