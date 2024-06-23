4c787b8d00cc64c6e79cec33fd17f953f03cab5d
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-06-23 14:32:45 +0200

    Set the webfont view vars in the FrontendView, not FrontendController

--- awyiss/View/FrontendView.php
+++ awyiss/View/FrontendView.php
@@ -118,6 +118,7 @@ class FrontendView extends AppView {
 		$lo_twig->addGlobal('folder', '/' . ltrim($this->request->getAttribute('base'), '/'));
 		$lo_twig->addGlobal('languages', LocaleMiddleware::getLanguages());
 		$lo_twig->addGlobal('languageShortcode', $lo_frontendLanguage?->shortcode);
+		$lo_twig->addGlobal('webfont', $this->getWebfontData());
 	}
 
 
@@ -468,5 +469,36 @@ class FrontendView extends AppView {
 	 */
 	public static function setRowClass(string $rowClass): void {
 		self::$rowClass = $rowClass;
+	}
+
+
+	/**
+	 * Get the data for the webfont from the design settings
+	 *
+	 * @return array
+	 */
+	protected function getWebfontData(): array {
+		/** @var \Awyiss\Middleware\DesignMiddleware $lo_designMiddleware */
+		$lo_designMiddleware = $this->getRequest()->getAttribute('design');
+		$la_variables = $lo_designMiddleware->getDesignVariables();
+
+		$la_webfontData = [];
+
+		if (!$la_variables) {
+			return $la_webfontData;
+		}
+
+		foreach ($la_variables as $ls_variable => $lx_value) {
+			if (!is_array($lx_value) || !isset($lx_value['font']['name'])) {
+				continue;
+			}
+
+			$la_webfontData[ $ls_variable ] = [
+				'name' => $lx_value['font']['name'],
+				'variants' => $lx_value['variants'] ?? [],
+			];
+		}
+
+		return $la_webfontData;
 	}
 }
