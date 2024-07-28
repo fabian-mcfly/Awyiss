53f94e341c93d7e8ea525d6e96f3e4e2206568b7
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-07-28 14:13:43 +0200

    Correctly sort the assigned elements

--- awyiss/Controller/Backend/ContentTemplatesController.php
+++ awyiss/Controller/Backend/ContentTemplatesController.php
@@ -315,6 +315,7 @@ class ContentTemplatesController extends Controller {
 			// Compare the positions
 			return $lx_aPos <=> $lx_bPos;
 		});
+		$la_availableContentAttributes = array_combine(array_column($la_availableContentAttributes, 'identifier'), $la_availableContentAttributes);
 
 		$la_columnSpans = $this->ContentTemplates->ContentTemplateElements->getColumnSpans();
 		$la_columnSpans = array_map(function (ColumnInterface $column): string {
