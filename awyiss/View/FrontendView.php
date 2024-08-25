1fd0b6e3ebeadb1a5c5b03de93c98bbc578fe427
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2024-08-25 12:45:14 +0200

    Make sure to not cache the webfont file

--- awyiss/View/FrontendView.php
+++ awyiss/View/FrontendView.php
@@ -128,6 +128,10 @@ class FrontendView extends AppView {
 		$lo_twig->addGlobal('languages', LocaleMiddleware::getLanguages());
 		$lo_twig->addGlobal('languageShortcode', $lo_frontendLanguage?->shortcode);
 		$lo_twig->addGlobal('webfont', $this->getWebfontData());
+
+		// Add the webfont timestamp to the global variables
+		$ls_webfontFilePath = ROOT . DS . CUSTOM_DIR . DS . 'assets' . DS . 'css' . DS . 'webfonts.css';
+		$lo_twig->addGlobal('webfontTimestamp', file_exists($ls_webfontFilePath) ? filemtime($ls_webfontFilePath) : null);
 	}
 
 
