a5f55c630e1c3281c8b746f603635e27c0ecaac6
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-03-23 14:37:32 +0100

    Use the db title as label for page role enum cases when no translation exists

--- awyiss/Model/Trait/PageRoleEnumTrait.php
+++ awyiss/Model/Trait/PageRoleEnumTrait.php
@@ -6,6 +6,7 @@ namespace Awyiss\Model\Trait;
 
 use Awyiss\Model\Enum\PageRoleEnumInterface;
 use Awyiss\Utility\Inflector;
+use Cake\Datasource\FactoryLocator;
 
 
 /**
@@ -32,6 +33,16 @@ trait PageRoleEnumTrait {
 	 * @inheritDoc
 	 */
 	public function label(): string {
+		$ls_headline = __d($this->name, 'headline_overview');
+		if (!str_contains($ls_headline, '::')) {
+			return $ls_headline;
+		}
+
+		$la_pageRoles = $this->fetchPageRoles();
+		if (isset($la_pageRoles[ Inflector::underscore($this->name) ])) {
+			return $la_pageRoles[ Inflector::underscore($this->name) ]->label;
+		}
+
 		return Inflector::humanize(Inflector::underscore($this->name));
 	}
 
@@ -49,5 +60,24 @@ trait PageRoleEnumTrait {
 	 */
 	public function tableName(): string {
 		return Inflector::underscore(Inflector::pluralize($this->name));
+	}
+
+
+	/**
+	 * @return array<\Awyiss\Model\Entity\PageRole>
+	 */
+	protected function fetchPageRoles(): array {
+		static $la_pageRoles;
+
+		if (isset($la_pageRoles)) {
+			return $la_pageRoles;
+		}
+
+		$lo_tableLocator = FactoryLocator::get('Table');
+
+		/** @var array<\Awyiss\Model\Entity\PageRole> $la_pageRoles */
+		$la_pageRoles = $lo_tableLocator->get('PageRoles')->find()->all()->indexBy('identifier')->toArray();
+
+		return $la_pageRoles;
 	}
 }
