5f47e2328ed9cd9d416b0911ab180c464559112f
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-03-30 11:34:36 +0200

    Added `orestbida/cookieconsent` as a default consent tool. Third party scripts should be set in `scripts_third_party.twig`

---
+++ awyiss/assets/js/Frontend/CookieConsent/cookieconsent-config.js
@@ -0,0 +1,108 @@
+(function (customConfig) {
+	const languageResolver = async(lang) => {
+		const url = `${baseUrl}awyiss/assets/js/Frontend/CookieConsent/locale/${lang}.json`
+
+		const res = await fetch(url);
+		const json = await res.json();
+
+		// Traverse through json.preferencesModal.sections (if present)
+		// and remove every object that has a `linkedCategory` property
+		// that is not present in the categories object
+		const categories = window.cookieBannerConfig.categories || {};
+		const sections = json.preferencesModal.sections || [];
+
+		json.preferencesModal.sections = sections.filter(section => {
+			if (!section.linkedCategory) {
+				return true;
+			}
+
+			return categories[section.linkedCategory];
+		});
+
+		return json;
+	}
+
+	const defaultConfig = {
+		guiOptions: {
+			consentModal: {
+				layout: 'cloud inline',
+				position: 'bottom right',
+				equalWeightButtons: false,
+				flipButtons: false
+			},
+			preferencesModal: {
+				layout: 'box',
+				position: 'right',
+				equalWeightButtons: false,
+				flipButtons: false
+			}
+		},
+		language: {
+			autoDetect: 'document',
+			default: 'en',
+			translations: {
+				'de': async() => { return languageResolver('de') },
+				'en': async() => { return languageResolver('en') },
+				'es': async() => { return languageResolver('es') },
+				'fr': async() => { return languageResolver('fr') },
+				'it': async() => { return languageResolver('it') },
+			}
+		}
+	};
+
+	// Merge the provided config with the default one
+	const config = Object.assign(defaultConfig, customConfig);
+	const categories = config.categories || {};
+
+	if (customConfig.autoDetectCategories) {
+		const scriptTags = Array.from(document.getElementsByTagName('script'));
+		scriptTags.forEach(tag => {
+			if (tag.type !== 'text/plain') {
+				// Check if the data-category is "necessary"
+				// If it is, add it to the categories object
+				const category = tag.getAttribute('data-category');
+
+				if (category === 'necessary') {
+					categories[ category ] = {
+						enabled: true,
+						readOnly: true,
+					};
+				}
+
+				return;
+			}
+
+			const category = tag.getAttribute('data-category');
+
+			if (!category || categories[category]) {
+				return;
+			}
+
+			categories[category] = {
+				enabled: ['legitimate-interest'].includes(category),
+				readOnly: false,
+			}
+
+			if (category === 'analytics') {
+				categories[category].autoClear = {
+					cookies: [{ name: /^(_ga)/ }, { name: '_gid'}],
+				}
+			}
+		});
+
+		config.categories = categories;
+	}
+
+	// Only run the CookieConsent script if there are categories defined
+	if (Object.keys(categories).length === 0) {
+		return;
+	}
+
+	window.cookieBannerConfig = config;
+
+	CookieConsent.run(config);
+
+	if (config.darkMode) {
+		document.documentElement.classList.add('cc--darkmode');
+	}
+})(cookieBannerConfig);
