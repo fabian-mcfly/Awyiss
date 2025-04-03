1bc6b01330f33ee4c8562200e32fa8b953699c6b
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-04-03 11:00:55 +0200

    Fixed incorrect session save path (CakePHP bug)

--- awyiss/config/awyiss.php
+++ awyiss/config/awyiss.php
@@ -320,6 +320,7 @@ return [
 			'session.gc_divisor' => 1000,
 			'session.gc_maxlifetime' => 86400, //Time in seconds!
 			'session.gc_probability' => 1,
+			'session.save_path' => (defined('TMP') ? TMP : sys_get_temp_dir() . DIRECTORY_SEPARATOR) . 'sessions',
 		],
 		'timeout' => 1440, //Time in minutes!
 	],
