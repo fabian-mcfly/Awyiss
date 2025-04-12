7aca6af9891f2037cf9b43b781d195d43045e65a
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-04-12 18:35:44 +0200

    Added the config options for queued jobs to allow changing the items per page setting

---
+++ awyiss/Configuration/ConfigOptions/QueuedJobsConfigOptions.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+
+namespace Awyiss\Configuration\ConfigOptions;
+
+
+use Awyiss\Awyiss;
+use Awyiss\Configuration\AbstractConfigOptions;
+use Awyiss\Configuration\ConfigOption;
+use Awyiss\Configuration\ConfigOptionType;
+
+
+/**
+ * Provides all configuration options for the queued jobs scope
+ */
+class QueuedJobsConfigOptions extends AbstractConfigOptions {
+	/**
+	 * @var string Scope of these options
+	 */
+	protected static string $scope = 'QueuedJobs';
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function initializeConfigOptions(): void {
+		$this->add(Awyiss::REALM_BACKEND, [
+			'paginate' => [
+				new ConfigOption(
+					defaultValue: 20,
+					identifier: 'limit',
+					localizable: false,
+					nullable: false,
+					personalizable: true,
+					type: ConfigOptionType::Integer,
+				),
+			],
+		]);
+	}
+}
