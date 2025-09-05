f80bbe3e43c90e110235f7e550e9a1e167cc0732
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-09-05 22:13:24 +0200

    Streamlined the force flag when moving a template

--- awyiss/Event/Backend/PageTemplatesListener.php
+++ awyiss/Event/Backend/PageTemplatesListener.php
@@ -46,14 +46,14 @@ class PageTemplatesListener implements EventListenerInterface {
 	 *
 	 * If such an active job exists, stop the save event and return an error.
 	 *
-	 * This is neccesary since a second file rename job could interfere with the first one.
+	 * This is necessary since a second file rename job could interfere with the first one.
 	 *
 	 * @param Event $event
 	 * @param PageTemplate $entity
 	 * @return void
 	 */
 	public function beforeSave(Event $event, PageTemplate $entity): void {
-		if ($entity->hasOriginal('fileName') && $entity->fileName != $entity->getOriginal('fileName')) {
+		if ($entity->hasOriginal('fileName') && $entity->get('fileName') != $entity->getOriginal('fileName')) {
 			/** @var \Queue\Model\Table\QueuedJobsTable $lo_queue */
 			$lo_queue = FactoryLocator::get('Table')->get('Queue.QueuedJobs');
 
@@ -73,9 +73,10 @@ class PageTemplatesListener implements EventListenerInterface {
 	 * @param \Cake\Event\Event $event
 	 * @param \Awyiss\Model\Entity\PageTemplate $entity
 	 * @param \ArrayObject $options
+	 * @noinspection DuplicatedCode, PhpUnusedParameterInspection
 	 */
 	public function afterSaveCommit(Event $event, PageTemplate $entity, ArrayObject $options): void {
-		$ls_fileName = Text::slug($entity->fileName, ['replacement' => '_']);
+		$ls_fileName = Text::slug($entity->get('fileName'), ['replacement' => '_']);
 		$ls_fileName = trim($ls_fileName, '_');
 		$ls_extension = '.twig';
 
@@ -90,20 +91,20 @@ class PageTemplatesListener implements EventListenerInterface {
 
 		$ls_filePath = $ls_folderPath . $ls_fileName . $ls_extension;
 
-		if (!($options['isCopy'] ?? false) && $entity->hasOriginal('fileName') && $entity->fileName != $entity->getOriginal('fileName')) {
+		if (!($options['isCopy'] ?? false) && $entity->hasOriginal('fileName') && $entity->get('fileName') != $entity->getOriginal('fileName')) {
 			//After changing the filename in the database, we also need to move (read: rename) the existing file
 			$ls_currentFileName = Text::slug($entity->getOriginal('fileName'), ['replacement' => '_']);
 			$ls_currentFilePath = $ls_folderPath . $ls_currentFileName . $ls_extension;
 			$lb_fileExists = file_exists($ls_currentFilePath);
 			if ($lb_fileExists) {
-				$la_commands[] = 'mv ' . $ls_currentFilePath . ' ' . $ls_filePath . ' --force';
+				$la_commands[] = 'mv -f ' . $ls_currentFilePath . ' ' . $ls_filePath;
 			}
 		}
 		else {
 			$lb_fileExists = file_exists($ls_filePath);
 		}
 
-		//If the file does not exist, we create one based on a twig-template for frontent page templates
+		//If the file does not exist, we create one based on a twig-template for frontend page templates
 		if (!$lb_fileExists) {
 			$la_commands[] = 'bin' . DS . 'cake bake template page_templates page_template ' . $ls_fileName . ' --prefix Frontend --controller page';
 			$la_commands[] = 'chmod 0755 ' . $ls_filePath;
@@ -150,13 +151,13 @@ class PageTemplatesListener implements EventListenerInterface {
 		if (file_exists($ls_filePath)) {
 			$ls_newFilePath = $ls_filePath;
 			while (file_exists($ls_newFilePath)) {
-				$ls_newFilePath = $ls_folderPath . '_deleted-' . $ls_fileName . '-' . (new DateTime())->getTimestamp() . $ls_extension;
+				$ls_newFilePath = $ls_folderPath . '_deleted-' . $ls_fileName . '-' . new DateTime()->getTimestamp() . $ls_extension;
 			}
 
 			/** @var \Queue\Model\Table\QueuedJobsTable $lo_queue */
 			$lo_queue = FactoryLocator::get('Table')->get('Queue.QueuedJobs');
 			$lo_queue->createJob('Queue.Execute', [
-				'command' => 'mv ' . $ls_filePath . ' ' . $ls_newFilePath,
+				'command' => 'mv -f ' . $ls_filePath . ' ' . $ls_newFilePath,
 				'log' => true,
 			], [
 				'group' => 'general',
