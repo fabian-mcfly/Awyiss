80366c19eb77d039379edd888ec9530160683b3c
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-09-12 14:39:51 +0200

    Updated comment

--- _customer_skeleton/config/environment_skeleton.php
+++ _customer_skeleton/config/environment_skeleton.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
 return [
-	/*
+	/**
 	 * Connection information used by the ORM to connect
 	 * to your application's datastores.
 	 *
-	 * See awyiss/config/awyiss.php for more configuration options.
+	 * @see /awyiss/config/awyiss.php for more configuration options.
 	 */
 	'Datasources' => [
 		'default' => [
