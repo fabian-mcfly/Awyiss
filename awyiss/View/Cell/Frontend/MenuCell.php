762c58c472cd13976839743b8468c134039d8f02
fabian-mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2025-09-29 23:03:48 +0200

    Handle missing menu records

--- awyiss/View/Cell/Frontend/MenuCell.php
+++ awyiss/View/Cell/Frontend/MenuCell.php
@@ -113,7 +113,7 @@ class MenuCell extends Cell {
 		/** @see \Awyiss\Utility\Menu\FrontendMenu::__construct() */
 		$lo_menu = new $ls_menuClass($lo_menuEntries->toArray(), [
 			'active' => $lb_active,
-			'identifier' => $lo_menuRecord->identifier ? Inflector::ucparts($lo_menuRecord->identifier) : null,
+			'identifier' => $lo_menuRecord?->identifier ? Inflector::ucparts($lo_menuRecord->identifier) : null,
 			'menuClass' => $ls_menuClass,
 			'menuItemClass' => $ls_menuItemClass,
 		]);
