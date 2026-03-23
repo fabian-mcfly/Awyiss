5d7b9dc48d0dea78f82f686fb2db94a6e020a314
fabian.mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2026-03-23 21:46:51 +0100

    Handle values of time DateTime and convert them to correct timezone and format to fix Chrome/Edge issues with the readable form the string casting would create

--- awyiss/View/StringTemplate.php
+++ awyiss/View/StringTemplate.php
@@ -4,8 +4,12 @@
 namespace Awyiss\View;
 
 
+use Awyiss\Awyiss;
 use Awyiss\Core\Configure\Engine\PhpConfig;
+use Awyiss\Middleware\LocaleMiddleware;
+use Cake\Core\Configure;
 use Cake\Core\Exception\CakeException;
+use Cake\I18n\DateTime;
 use Cake\View\StringTemplate as BaseStringTemplate;
 
 
@@ -25,5 +29,30 @@ class StringTemplate extends BaseStringTemplate {
 
 		$loader = new PhpConfig();
 		$this->add($loader->read($file));
+	}
+
+
+	/**
+	 * Re-implemented to handle DateTime objects and convert them to the correct timezone before formatting
+	 *
+	 * @inheritDoc
+	 */
+	public function formatAttributes(?array $options, ?array $exclude = null): string {
+		if (isset($options['value']) && $options['value'] instanceof DateTime) {
+			$timezone = ($options['timezone'] ?? null) ?: Configure::read('Awyiss.System.' . Awyiss::getRealm() . '.timezone') ?: date_default_timezone_get(); // phpcs:ignore
+
+			if ($timezone === 'auto') {
+				$language = $options['language'] ?? LocaleMiddleware::getLanguage(null);
+				$timezone = $language->timezone;
+			}
+
+			if ($timezone) {
+				$options['value'] = $options['value']->setTimezone($timezone);
+			}
+
+			$options['value'] = $options['value']->format('Y-m-d\TH:i:s');
+		}
+
+		return parent::formatAttributes($options, $exclude);
 	}
 }
