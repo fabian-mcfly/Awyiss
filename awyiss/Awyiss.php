69202ff594447b954947fca8546a2c7cf2689dd9
fabian.mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2026-07-25 00:57:40 +0200

    Updated version to 0.2.5

--- awyiss/Awyiss.php
+++ awyiss/Awyiss.php
@@ -53,7 +53,7 @@ class Awyiss extends BaseApplication {
 	/**
 	 * The version of Awyiss
 	 */
-	final public const string VERSION = '0.2.0';
+	final public const string VERSION = '0.2.5';
 	/**
 	 * The name of the version
 	 */
