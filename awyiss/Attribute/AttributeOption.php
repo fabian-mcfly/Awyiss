674248cc130b129dd6263668fbed3c5ee4bfe9fe
fabian.mcfly <13197057+fabian-mcfly@users.noreply.github.com>
2026-09-05 23:17:24 +0200

    Correct style

--- awyiss/Attribute/AttributeOption.php
+++ awyiss/Attribute/AttributeOption.php
@@ -168,7 +168,7 @@ class AttributeOption {
 
 
 	/**
-	 * @param callable|bool|array $disabled
+	 * @param callable|array|bool $disabled
 	 * @return self
 	 * @noinspection PhpUnused
 	 */
@@ -281,6 +281,7 @@ class AttributeOption {
 	 * @param bool $evaluate Whether callable values should be evaluated.
 	 * @param \Awyiss\Model\Entity|null $entity Entity used for evaluation.
 	 * @param array $currentOptions Options collected during evaluation.
+	 * @return mixed
 	 * @noinspection PhpUnused
 	 */
 	public function getValue(bool $evaluate = false, ?Entity $entity = null, array &$currentOptions = []): mixed {
